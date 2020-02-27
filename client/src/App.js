65341a71e7834d8527ba61e76c8cf04abfc76bc5
Damian Obajtek <damian.obajtek@gmail.com>
2020-02-27 21:23:36 +0100

    added simple react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-import App from "./App";
+import Chat from "./components/Chat";
+import Join from "./components/Join";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" exact component={Chat} />
+  </Router>
+);
+
+export default App;
