af987f302dabf229960d8be14677815aeaad1cd8
Damian Obajtek <damian.obajtek@gmail.com>
2020-03-30 16:53:37 +0200

    made router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen();
+const router = require(`./router`);
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
