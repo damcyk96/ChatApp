eeac930d26b1989f69fcad2e0b2186851d7b6143
Damian Obajtek <damian.obajtek@gmail.com>
2020-04-02 13:49:25 +0200

    added messages function

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,8 @@ import io from "socket.io-client";
 
 import "./Chat.css";
 import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
 
 let socket;
 
@@ -47,13 +49,12 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-        {/* <input
-          value={message}
-          onChange={event => setMessage(event.target.value)}
-          onKeyPress={event =>
-            event.key === "Enter" ? sendMessage(event) : null
-          }
-        /> */}
+        <Messages messages={messages} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
     </div>
   );
