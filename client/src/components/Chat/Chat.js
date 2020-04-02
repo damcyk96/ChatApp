29be741318bba7ec758fdb87a4cbef0ed7a5038e
Damian Obajtek <damian.obajtek@gmail.com>
2020-04-02 12:48:10 +0200

    added infobar

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import queryString from "query-string";
 import io from "socket.io-client";
 
 import "./Chat.css";
+import InfoBar from "../InfoBar/InfoBar";
 
 let socket;
 
@@ -43,15 +44,16 @@ const Chat = ({ location }) => {
   console.log(message, messages);
 
   return (
-    <div className="outerContainter">
+    <div className="outerContainer">
       <div className="container">
-        <input
+        <InfoBar room={room} />
+        {/* <input
           value={message}
           onChange={event => setMessage(event.target.value)}
           onKeyPress={event =>
             event.key === "Enter" ? sendMessage(event) : null
           }
-        />
+        /> */}
       </div>
     </div>
   );
